4a92862eba707c1a81902f67615925842f6ed293
J <jaumpc@gmail.com>
2021-08-30 16:25:41 -0300

    Mudanças fps

--- Assets/_Scripts/DoorOpen.cs
+++ Assets/_Scripts/DoorOpen.cs
@@ -4,31 +4,16 @@ using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
 {
-    public CanvasGroup openUI;
+    
     public Animator DoorAnimator;
 
 
-    public void OnButtonClick1()
-    {
-        openUI.alpha = 0;
-        DoorAnimator.SetBool("open", true);
-    }
+
  
     private void OnTriggerEnter(Collider other)
     {
-       
+       DoorAnimator.SetBool("open", true);
+       Destroy(gameObject);
     }
-    private void OnTriggerStay(Collider other)
-    {
-        if(other.tag == "battery"){
-           openUI.alpha = 1f;
-           openUI.blocksRaycasts = true; 
-        }
-     
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        openUI.alpha = 0;
-        
-    }
+    
 }
