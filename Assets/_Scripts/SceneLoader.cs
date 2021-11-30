671d144d69b212e06f1d5057d864dfa2ec85402b
A <jaumpc@gmail.com>
2021-11-29 22:34:38 -0300

    crossfade + minor changes

--- Assets/_Scripts/SceneLoader.cs
+++ Assets/_Scripts/SceneLoader.cs
@@ -5,13 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+
+    public Animator transition;
     
     public void LoadNextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
     public void ExitGame() {
         Application.Quit();
     }
+
+    IEnumerator LoadLevel(int levelIndex){
+
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(levelIndex);
+
+
+    }
 }
+
